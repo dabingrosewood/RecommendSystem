9c5c077b04d73285644f95b0ab57403fb763febf
YixiaoTang <31689524+YixiaoTang@users.noreply.github.com>
2017-09-16 00:24:08 +0200

    自己写的读取与老师给的例子结合
    前一段程序是自己写的
    后半段老师给的
    主要修改点在格式，
    为了方便程序，把ID rate 全部同一成了float格式
    本来想分别改成int float的  但是没成功  就直接全部float了。
    
    下一步可以开始写算法一了

--- AiDM_assignment1.py
+++ AiDM_assignment1.py
@@ -6,3 +6,43 @@ with open("E:\AiDM\Assignment1\ml-latest-small\ml-latest-small\\ratings.csv", "r
     data=[]
     for i in read:
         data.append(i)
+    data.pop(0)
+    ratings=np.array(data)
+
+    # split data into 5 train and test folds
+    nfolds = 5
+
+    # allocate memory for results:
+    err_train = np.zeros(nfolds)
+    err_test = np.zeros(nfolds)
+
+    # to make sure you are able to repeat results, set the random seed to something:
+    np.random.seed(17)  # 方法改变随机数生成器的种子
+
+    seqs = [x % nfolds for x in range(len(ratings))]
+    np.random.shuffle(seqs)  # 方法将序列的所有元素随机排序,seq保存每一个样本的归属编号（1-nflod）
+
+    # for each fold:
+    for fold in range(1):
+        train_sel = np.array([x != fold for x in seqs])
+        test_sel = np.array([x == fold for x in seqs])
+        train = ratings[train_sel]
+        test = ratings[test_sel]
+        test = test.astype(np.float)
+        train = train.astype(np.float)
+       # calculate model parameters: mean rating over the training set:
+        gmr = np.mean(train[:,2])  # global mean rating
+
+        # apply the model to the train set:
+        err_train[fold] = np.sqrt(np.mean((train[:, 2] - gmr) ** 2))
+
+        # apply the model to the test set:
+        err_test[fold] = np.sqrt(np.mean((test[:, 2] - gmr) ** 2))
+
+        # print errors:
+        print("Fold " + str(fold) + ": RMSE_train=" + str(err_train[fold]) + "; RMSE_test=" + str(err_test[fold]))
+        # print the final conclusion:
+        print("\n")
+        print("Mean error on TRAIN: " + str(np.mean(err_train)))
+        print("Mean error on  TEST: " + str(np.mean(err_test)))
+
