ee70bfe0b088922ebb61df4ebb4700551cdab471
YixiaoTang <31689524+YixiaoTang@users.noreply.github.com>
2017-09-19 00:43:05 +0200

    assignment1 naive 算法
    对应于四个公式中的2 3 4
    第一个老师已经直接给出
    naive_3程序结果有误 每一次fold时误差值都会逐渐加大。
    需要更进一步调试
    naive_2 和naive_4的结果与网络上的标准结果做过比较，略差，应该是正确的情况。
    下一步打算更改naive_4中的fallback value。 或许可以利用avg_user 或者 avg_item代替全局均值。

---
+++ assignment1_naive_4.py
@@ -0,0 +1,75 @@
+import numpy as np
+ratings = np.genfromtxt("E:\AiDM\Assignment1\ml-1m\ml-1m\\ratings.dat", usecols=(0, 1, 2), delimiter='::', dtype='int')
+nfolds = 5
+# allocate memory for results:
+err_train = np.zeros(nfolds)
+err_test = np.zeros(nfolds)
+MAE_train = np.zeros(nfolds)
+MAE_test = np.zeros(nfolds)
+# to make sure you are able to repeat results, set the random seed to something:
+np.random.seed(28)
+max_user = np.max(ratings[:,0])
+max_item = np.max(ratings[:,1])
+eachnum_user = np.zeros(max_user+1)  # the number of rate of each user or item
+eachnum_item = np.zeros(max_item+1)
+avg_user = np.zeros(max_user+1)
+avg_item = np.zeros(max_item+1)
+seqs = [x % nfolds for x in range(len(ratings))]
+np.random.shuffle(seqs)
+for fold in range(nfolds):
+    train_sel = np.array([x != fold for x in seqs])
+    test_sel = np.array([x == fold for x in seqs])
+    train = ratings[train_sel]
+    test = ratings[test_sel]
+    test = test.astype(np.float)
+    train = train.astype(np.float)
+    gmr = np.mean(train[:, 2])
+    for x in range(len(train)): #caculate average of users and average of items
+        eachnum_user[train[x][0]] = eachnum_user[train[x][0]]+1
+        eachnum_item[train[x][1]] = eachnum_item[train[x][1]]+1
+        avg_user[train[x][0]] = avg_user[train[x][0]]+train[x][2]
+        avg_item[train[x][1]] = avg_item[train[x][1]]+train[x][2]
+    # find the item or the user which has no sample in the train set
+    tmp = np.array([x == 0 for x in eachnum_user])
+    avg_user[tmp] = gmr
+    eachnum_user[tmp] = 1
+    avg_user = avg_user / eachnum_user
+    tmp = np.array([x == 0 for x in eachnum_item])
+    eachnum_item[tmp] = 1
+    avg_item = avg_item / eachnum_item
+    X1 = np.zeros(len(train))
+    X2 = np.zeros(len(train))
+    for x in range(len(train)):
+        X1[x] = avg_user[train[x][0]]
+        X2[x] = avg_item[train[x][1]]
+    Y = train[:,2]
+    X = np.vstack([X1, X2, np.ones(len(train))]).T
+    S = np.linalg.lstsq(X, Y)
+    print(S[0])
+    pred_train = np.zeros(len(train))
+    for x in range(len(train)):
+        pred_train[x] = X1[x] * S[0][0] + X2[x] * S[0][1] + S[0][2]
+    err_train[fold] = np.sqrt(np.mean((train[:, 2] - pred_train) ** 2))
+    MAE_train[fold] = np.mean(np.abs(train[:, 2] - pred_train))
+    # test phase
+    X1 = np.zeros(len(test))
+    X2 = np.zeros(len(test))
+    for x in range(len(test)):
+        X1[x] = avg_user[test[x][0]]
+        X2[x] = avg_item[test[x][1]]
+    Y = test[:, 2]
+    X = np.vstack([X1, X2, np.ones(len(test))]).T
+    S = np.linalg.lstsq(X, Y)
+    pred_test = np.zeros(len(test))
+    for x in range(len(test)):
+        pred_test[x] = X1[x] * S[0][0] + X2[x] * S[0][1] + S[0][2]
+    err_test[fold] = np.sqrt(np.mean((test[:, 2] - pred_test) ** 2))
+    MAE_test[fold] = np.mean(np.abs(test[:, 2] - pred_test))
+print('RMSE of train:')
+print(err_train)
+print('RMSE of test:')
+print(err_test)
+print('MAE of train:')
+print(MAE_train)
+print('MAE of test:')
+print(MAE_test)
